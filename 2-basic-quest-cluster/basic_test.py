7b4d604979ca48d188a879c69aad642f679a21ee
Bradly Stadie <bstadie@vectorinstitute.ai>
2022-09-29 20:55:53 -0500

    nc

---
+++ 2-basic-quest-cluster/basic_test.py
@@ -0,0 +1,9 @@
+
+def main():
+    with open('test_log.txt', 'w') as f:
+        f.write('Test log completed successfully.')
+        f.write("\nLoss is 0.0")
+
+
+if __name__ == "__main__":
+    main()
